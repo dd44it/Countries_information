5415c46b1c0582d6104c82be1d5f71c7c839077c
dd44it <12nelson13@gmail.com>
2023-04-14 21:51:43 +0300

    feat: add Header component and style

---
+++ src/components/Header.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+export default function Header(){
+  return (
+    <header className="header">
+      <div className="container">
+        <div className="header-wrapper">
+          <h1 className="title">Where in the world?</h1>
+          <div className="theme">
+            Dark Mode
+          </div>
+        </div>
+      </div>
+    </header>
+  )
+}
--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,9 +1,11 @@
 import React from "react"
 import { Outlet } from "react-router-dom"
+import Header from "./Header"
 
 export default function Layout() {
   return (
     <div className="site-wrapper">
+      <Header />
       <main className="main">
         <Outlet />
       </main>
