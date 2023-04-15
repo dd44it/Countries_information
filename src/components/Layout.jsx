ef38efbf00de1da643b57b36255bc1db3eba1a20
dd44it <12nelson13@gmail.com>
2023-04-15 17:57:27 +0300

    feat: add static select element

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -8,10 +8,20 @@ export default function Layout() {
       <Header />
       <main className="main">
         <div className="container">
-          <section className="search">
+          <section className="filter">
             <div className="search-wrapper">
               <span className="icon-search"></span>
               <input type="text" className="search-input" placeholder="Search for a country..." />
+            </div>
+            <div className="select-wrapper">
+              <select id="" className="choose-select">
+                <option value="">Filter by Region</option>
+                <option value="Africa">Africa</option>
+                <option value="America">America</option>
+                <option value="Asia">Asia</option>
+                <option value="Europe">Europe</option>
+                <option value="Oceania">Oceania</option>
+              </select>
             </div>
           </section>
 
