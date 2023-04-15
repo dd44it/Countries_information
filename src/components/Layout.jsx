083a6528a01165a375dda5b39412f5694c48bd3f
dd44it <12nelson13@gmail.com>
2023-04-15 18:43:05 +0300

    feat: add section with cards coutnries

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,8 +1,43 @@
-import React from "react"
+import React, { useEffect } from "react"
 import { Outlet } from "react-router-dom"
 import Header from "./Header"
+import data  from "../data.json"
 
 export default function Layout() {
+  console.log(data)
+
+  // useEffect(() => {
+
+  // }, [])
+
+  const cardElem = data.map( (item, index) => {
+    return (
+        <div className="card-country" key={index}>
+          <div className="card-img"> 
+            <img src={item.flag} alt="" />
+          </div>
+          <div className="card-body">
+            <h3 className="card-title"> {item.name} </h3>
+            <div className="card-info">
+              <div className="card-info-row">
+                <div className="card-label">Population</div>
+                <div className="card-population"> {item.population} </div>
+              </div>
+              <div className="card-info-row">
+                <div className="card-label">Region</div>
+                <div className="card-region">{item.region}</div>
+              </div>
+              <div className="card-info-row">
+                <div className="card-label">Capital</div>
+                <div className="card-capital">{item.capital}</div>
+              </div>
+            </div>
+          </div>
+        </div>
+      )
+  })
+
+
   return (
     <div className="site-wrapper">
       <Header />
@@ -24,7 +59,9 @@ export default function Layout() {
               </select>
             </div>
           </section>
-
+          <section className="countries">
+            {cardElem}
+          </section>
           <Outlet />
         </div>
       </main>
