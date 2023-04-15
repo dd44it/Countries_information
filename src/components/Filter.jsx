0085fb8b0392bd9d54890103a663994de1773c6b
dd44it <12nelson13@gmail.com>
2023-04-15 21:29:41 +0300

    refactor: add Filter component

---
+++ src/components/Filter.jsx
@@ -0,0 +1,30 @@
+import React from "react"
+
+export default function Filter({country, handleText, searchCountry}) {
+  return (
+    <section className="filter">
+      <div className="search-wrapper">
+        <span className="icon-search"></span>
+        <input
+          type="text"
+          className="search-input"
+          placeholder="Search for a country..."
+          name="country"
+          value={country}
+          onChange={handleText}
+          onKeyPress={searchCountry}
+        />
+      </div>
+      <div className="select-wrapper">
+        <select id="" className="choose-select">
+          <option value="">Filter by Region</option>
+          <option value="Africa">Africa</option>
+          <option value="America">America</option>
+          <option value="Asia">Asia</option>
+          <option value="Europe">Europe</option>
+          <option value="Oceania">Oceania</option>
+        </select>
+      </div>
+    </section>
+  )
+}
--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -2,6 +2,7 @@ import React, { useState } from "react"
 import { Outlet } from "react-router-dom"
 import Cards from "./Cards"
 import Header from "./Header"
+import Filter from "./Filter"
 import data from "../data.json"
 
 export default function Layout() {
@@ -32,30 +33,11 @@ export default function Layout() {
       <Header />
       <main className="main">
         <div className="container">
-          <section className="filter">
-            <div className="search-wrapper">
-              <span className="icon-search"></span>
-              <input
-                type="text"
-                className="search-input"
-                placeholder="Search for a country..."
-                name="country"
-                value={country}
-                onChange={handleText}
-                onKeyPress={searchCountry}
-              />
-            </div>
-            <div className="select-wrapper">
-              <select id="" className="choose-select">
-                <option value="">Filter by Region</option>
-                <option value="Africa">Africa</option>
-                <option value="America">America</option>
-                <option value="Asia">Asia</option>
-                <option value="Europe">Europe</option>
-                <option value="Oceania">Oceania</option>
-              </select>
-            </div>
-          </section>
+          <Filter 
+            country={country}  
+            handleText={handleText}
+            searchCountry={searchCountry}
+           />
           <Cards listCountries={searchedCountries.length ? searchedCountries : countriesList} />
           <Outlet />
         </div>
