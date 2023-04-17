72578c29ba5fe3b79bdc1bd28f2be626dc7a8ac5
dd44it <12nelson13@gmail.com>
2023-04-17 16:33:04 +0300

    feat: add ThemeContext component

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,20 +1,18 @@
-/* eslint-disable no-undef */
 import React from "react"
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
 import { faMoon } from "@fortawesome/free-regular-svg-icons"
+import { ThemeContext } from "../ThemeContext"
 
 export default function Header() {
+  const {toggleTheme} = React.useContext(ThemeContext)
+
   return (
-    <header className="header">
-      <div className="container">
-        <div className="header-wrapper">
-          <h1 className="title">Where in the world?</h1>
-          <button className="theme">
-            <FontAwesomeIcon icon={faMoon} />
-            Dark Mode
-          </button>
-        </div>
-      </div>
-    </header>
+    <div className="header-wrapper">
+      <h1 className="title">Where in the world?</h1>
+      <button className="theme" onClick={toggleTheme}>
+        <FontAwesomeIcon icon={faMoon} />
+        Dark Mode
+      </button>
+    </div>
   )
 }
--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,12 +1,19 @@
-import React, { useState } from "react"
+import React from "react"
 import { Outlet } from "react-router-dom"
 import Header from "./Header"
+import { ThemeContext } from "../ThemeContext"
 
 export default function Layout() {
+  const { theme } = React.useContext(ThemeContext)
+
   return (
     <div className="site-wrapper">
-      <Header />
-      <main className="main">
+      <header className={`header header-${theme}`}>
+        <div className="container">
+          <Header />
+        </div>
+      </header>
+      <main className={`main main-${theme}`}>
         <div className="container">
           <Outlet />
         </div>
