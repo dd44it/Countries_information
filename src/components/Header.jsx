e17a52c2ad7542766a8cc2e9f484af2bc476323f
dd44it <12nelson13@gmail.com>
2023-04-17 13:11:48 +0300

    init: add fortawesome/free-regular-svg-icons

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,14 +1,18 @@
-import React from "react";
+/* eslint-disable no-undef */
+import React from "react"
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
+import { faMoon } from "@fortawesome/free-regular-svg-icons"
 
-export default function Header(){
+export default function Header() {
   return (
     <header className="header">
       <div className="container">
         <div className="header-wrapper">
           <h1 className="title">Where in the world?</h1>
-          <div className="theme">
+          <button className="theme">
+            <FontAwesomeIcon icon={faMoon} />
             Dark Mode
-          </div>
+          </button>
         </div>
       </div>
     </header>
