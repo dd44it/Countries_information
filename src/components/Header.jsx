3c5998231667a68e748618c0d4579cb446a37523
dd44it <12nelson13@gmail.com>
2023-04-18 13:20:38 +0300

    feat: add 2 mode theme icon

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,17 +1,18 @@
 import React from "react"
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
-import { faMoon } from "@fortawesome/free-regular-svg-icons"
+import { faMoon as faMoonRegular } from "@fortawesome/free-regular-svg-icons"
+import { faMoon as faMoonSolid } from "@fortawesome/free-solid-svg-icons"
 import { ThemeContext } from "../ThemeContext"
 
 export default function Header() {
-  const {toggleTheme} = React.useContext(ThemeContext)
+  const {theme, toggleTheme} = React.useContext(ThemeContext)
 
   return (
     <div className="header-wrapper">
       <h1 className="title">Where in the world?</h1>
       <button className="theme" onClick={toggleTheme}>
-        <FontAwesomeIcon icon={faMoon} />
-        Dark Mode
+        {theme === "light" ? <FontAwesomeIcon className="moon-icon" icon={faMoonRegular} /> : <FontAwesomeIcon className="moon-icon" icon={faMoonSolid} /> }
+        <span>Dark Mode</span>
       </button>
     </div>
   )
